bc09f674fb76fc55c61d8326dcc0b4e3d931bc26
amir413 <amir.emad2007@gmail.com>
2023-06-16 13:29:40 -0700

    sdsa

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+  * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char c;
