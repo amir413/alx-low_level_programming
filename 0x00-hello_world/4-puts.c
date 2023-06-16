39aa89910854d31c393bfa54a6d0e14aca0962aa
amir413 <amir.emad2007@gmail.com>
2023-06-15 18:11:57 -0700

    sas

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,7 @@
-include <stdio.h>
+#include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Retun: always 0 (Success)
- */
-
-int main()
-{  
-	puts ("Programming is like building a multilingual puzzle\n");
-	return 0;
+int main() {
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
 }
+
