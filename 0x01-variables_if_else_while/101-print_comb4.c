e0c444a682d20c1a61e9e8d126a8a67d9581271d
amir413 <amir.emad2007@gmail.com>
2023-06-16 16:24:18 -0700

    sdas

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstDigit;
+	int secondDigit;
+
+	for (firstDigit = 0; firstDigit < 9; firstDigit++)
+	{
+		for (secondDigit = firstDigit + 1; secondDigit < 10; secondDigit++)
+		{
+			putchar(firstDigit + '0');
+			putchar(secondDigit + '0');
+
+			if (firstDigit != 8 || secondDigit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
