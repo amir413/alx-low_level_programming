5cb0198ee716eabcced91328f1d86fc9efb28406
amir413 <amir.emad2007@gmail.com>
2023-06-16 15:54:37 -0700

    asd

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-	int i;
+	int letter;
 
-	for (i = 0; i < 10; i++)
+	for (letter = 'z'; letter >= 'a'; letter--)
 	{
-		putchar(i + '0');
+		putchar(letter);
 	}
+
 	putchar('\n');
 
-	return (0);
+	return 0;
 }
 
