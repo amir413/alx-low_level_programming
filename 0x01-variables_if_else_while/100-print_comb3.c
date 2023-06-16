d9ef4eeb17d5ca57b32dd9884683b453786e1015
amir413 <amir.emad2007@gmail.com>
2023-06-16 16:20:00 -0700

    asdas

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digits;
+
+	for (digits = 0; digits < 100; digits++)
+	{
+		if (digits % 10 > digits / 10)
+		{
+			putchar(digits / 10 + '0');
+			putchar(digits % 10 + '0');
+
+			if (digits != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
