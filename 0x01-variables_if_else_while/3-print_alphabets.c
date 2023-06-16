3c5046668fdc2a9d9e6de43e1e0b5550ee9bd22f
amir413 <amir.emad2007@gmail.com>
2023-06-16 13:47:33 -0700

    sda

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z' ; i++)
+	{
+		putchar(i)
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
