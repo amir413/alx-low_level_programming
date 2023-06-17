e23b4ecebea3cff782ef15bf5047d3ce00f03342
amir413 <amir.emad2007@gmail.com>
2023-06-16 17:20:10 -0700

    sdas

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
 
 /**
- * main - Entry poin
- * Description:write a program that prints all possible
- * different combinations of two digits
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
-
+#include<stdio.h>
 int main(void)
 {
-	int firstDigit = 0, seconDigit;
+    int firstDigit = 0 , seconDigit;
 
-	while (firstDigit <= 99)
-	{
-		seconDigit = firstDigit;
-		while (seconDigit <= 99)
-		{
-			if (seconDigit != firstDigit)
-			{
-				putchar((firstDigit / 10) + 48);
-				putchar((firstDigit % 10) + 48);
-				putchar(' ');
-				putchar((seconDigit / 10) + 48);
-				putchar((seconDigit % 10) + 48);
+    while (firstDigit <= 99)
+    {
+        seconDigit = firstDigit;
+        while (seconDigit <= 99)
+        {
+            if (seconDigit != firstDigit)
+            {
+                putchar((firstDigit / 10) + 48);
+                putchar((firstDigit % 10) + 48);
+                putchar(' ');
+                putchar((seconDigit / 10) + 48);
+                putchar((seconDigit % 10) + 48);
 
-				if (firstDigit !=98 || seconDigit != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			seconDigit++;
-		}
-		firstDigit++;
-	}
-	firstDigit++;
-}putchar('\n');
-return (0);
+                if (firstDigit !=98 || seconDigit != 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+            seconDigit++;
+        }
+        firstDigit++;
+    }
+    putchar('\n');
+    return (0);
 }
