569b6c6e8eff88276d66481c8f0567d85ccea9a4
amir413 <amir.emad2007@gmail.com>
2023-06-20 14:42:29 -0700

    sas

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
-int main() {
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The Character to print
+ * Return: On success l.
+ * On error, -l is returned, and errno is set appropriately.
+ */
+int main(void) {
     printf("_putchar\n");
-
     return 0;
 }
 
