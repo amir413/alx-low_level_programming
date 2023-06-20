b74a36f92759ced0edd91cf27cc5028758c08e68
amir413 <amir.emad2007@gmail.com>
2023-06-20 15:16:34 -0700

    sds

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,12 @@
-sasdasdasd
-asdas
+#include "main.h"
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
