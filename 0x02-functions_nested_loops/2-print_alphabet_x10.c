55d2e2baaa43fa12d968adb601be81c174a8c293
amir413 <amir.emad2007@gmail.com>
2023-06-20 15:38:07 -0700

    dsf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0.
  */
-int main() {
+int main(void)
+{
 	int i;
 	char alp;
 	for (i=0 ; i<10 ; i++){
@@ -14,5 +15,5 @@ int main() {
 		}
 		printf("\n");
 	}
-	return 0;
+	return (0);
 }
