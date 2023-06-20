e043e5799818d8c282e7cceaf4dd078b09b1a0da
amir413 <amir.emad2007@gmail.com>
2023-06-20 11:17:00 +0300

    sds

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main() {
+	int i;
+	char alp;
+	for (i=0 ; i<10 ; i++){
+		for (alp = 'a' ; alp <= z ; alp++){
+			printf ("%c ", alp);
+		}
+		printf("\n");
+	}
+	return 0;
+}
