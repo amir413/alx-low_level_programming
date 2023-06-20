52ea23080bfb8044b0ca720c1e0891a7c1920d66
amir413 <amir.emad2007@gmail.com>
2023-06-21 01:06:26 +0300

    sdsa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main() {
+int main()
+{
     char alp;
     for (alp = 'a'; alp <= 'z'; alp++) {
         printf("%c", alp);
     }
-    return 0;
+    return (0);
 }
 
