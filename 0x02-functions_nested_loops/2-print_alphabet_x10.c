2b732041818491887be218190cc4592b581816c8
amir413 <amir.emad2007@gmail.com>
2023-06-20 17:08:43 -0700

    asa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,12 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    int alp;
+	int line. ch;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (alp = 'a'; alp <= 'z'; alp++)
-        {
-            _putchar(alp);
-        }
-        _putchar('\n');
-    }
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n')
+	}
 }
-
