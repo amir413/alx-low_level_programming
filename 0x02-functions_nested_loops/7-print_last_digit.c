cd31efcae4e7342be9de27f9f4d0576d85f459ba
amir413 <amir.emad2007@gmail.com>
2023-06-20 19:05:35 -0700

    as

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
  */
 int print_last_digit(int number)
 {
-	int last_digit = number % 10;
-	_putchar('Last digit: %d\n', last_digit);
+	int print_last_digit = number % 10;
+	_putchar('Last digit: %d\n', print_last_digit);
 	return (last_digit);
 }
