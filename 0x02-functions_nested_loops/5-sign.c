f785558d292e854cfb8c91004cc3d9e852bc69f0
amir413 <amir.emad2007@gmail.com>
2023-06-20 18:33:06 -0700

    sa

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * @brief Prints the sign of a number.
+ *
+ * This function takes an integer as input and prints the sign of the number.
+ * - If the number is greater than zero, it prints '+' and returns 1.
+ * - If the number is less than zero, it prints '-' and returns -1.
+ * - If the number is zero, it prints '0' and returns 0.
+ *
+ * @param n The number for which the sign needs to be determined.
+ * @return The sign of the number: 1 if positive, -1 if negative, 0 if zero.
+ */
+int print_sign(int n)
+{
+	if(n < 0)
+	{
+		std::cout << '-';
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		st::cout << '+';
+		return (1);
+	}
+	else {
+		std::cout << '0';
+		return (0);
+	}
+}
+
