06c5ab9c1f78f7c7da056454698f405c9b4114bd
amir413 <amir.emad2007@gmail.com>
2023-06-22 03:47:39 +0300

    dd

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    if (a > 0)
+    {
+        printf ( a ,"%d number is postive");
+    }
+    else if (a < 0)
+    {
+        printf ( a ,"%d number is negative");
+    }
+    else
+    {
+        printf ( a ,"%d is zero");
+    }
+
+    return 0;
+}
+
