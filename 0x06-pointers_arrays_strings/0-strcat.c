2a6ea9f6253a7a4908584d866278a9935467a287
amir413 <amir.emad2007@gmail.com>
2023-06-29 08:52:04 -0700

    sa

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,40 +1,26 @@
 #include "main.h"
+#include <string.h>
 
-/*
- * _strcat - Concatenates two strings.
- * @dest: Pointer to the destination string.
- * @src: Pointer to the source string.
- * _strcat it is concatenates string2 to string1 and ends the
- * resulting string with the null character
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
  *
- * Description: This function appends the source string to the destination
- * string, overwriting the terminating null byte ('\0') at the end of dest,
- * and then adds a new terminating null byte.
- *
- * char *strcat(char *destination, const char *source)
- * Return: Pointer to the resulting string dest.
- *
- * _strcat it is concatenates string2 to string1 and ends the
- * resulting string with the null character
- * char *_strcat(char *dest, const char *src);
+ * Return: Pointer to the resulting string @dest
  */
-
-/* _strcat - Concatenates two strings.*/
-char *_strcat(char *dest, char *src)/* _strcat - Concatenates two strings.*/
-	/* _strcat - Concatenates two strings.*/
+char *_strcat(char *dest, char *src)
 {
-	int c, c2;
+	int dest_len = 0;
+	int i = 0;
 
-	c = 0;
-	/*find the size of dest array*/
-	while (dest[c])
-	c++;
-	/* iterate through each src array value without the null byte*/
-	for (c2 = 0; src[c2] ; c2++)
-	/*append src[c2] to dest[c] while overwritting the null byte in dest*/
-	dest[c++] = src[c2];
-
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	while (src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+	dest[dest_len] = '\0';
 	return (dest);
 }
-
-
