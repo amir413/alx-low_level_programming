d301b2358baa8e95d6a1c38e49e8640a67f490d8
amir413 <amir.emad2007@gmail.com>
2023-06-29 08:35:37 -0700

    sa

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,23 +12,23 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+    int i;
 
     /* Compare characters of s1 and s2 */
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-{
-	    	if (s1[i] < s2[i])
-		return -1;
-		else if (s1[i] > s2[i])
-		return 1;
-	}
+    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+    {
+        if (s1[i] < s2[i])
+            return -1;
+        else if (s1[i] > s2[i])
+            return 1;
+    }
 
     /* Check if s1 and s2 have different lengths */
-	if (s1[i] != '\0')
-	return 1;
-	else if (s2[i] != '\0')
-	return (-1);
+    if (s1[i] != '\0')
+        return 1;
+    else if (s2[i] != '\0')
+        return -1;
 
-	return (0); // Strings are equal
+    return 0; // Strings are equal
 }
 
