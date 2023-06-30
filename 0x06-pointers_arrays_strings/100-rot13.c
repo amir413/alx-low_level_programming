4308e728a7a181a5c9cc9c2c481dfc84e60d7803
amir413 <amir.emad2007@gmail.com>
2023-06-30 22:22:41 +0300

    as

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,22 +2,18 @@
 #include <string.h>
 
 char* rot13(char* str) {
-    int length = strlen(str);
-    char* encoded = (char*)malloc((length + 1) * sizeof(char));
+    char* encoded = strdup(str);  // Create a copy of the input string
 
-    for (int i = 0; i < length; i++) {
-        char c = str[i];
+    for (int i = 0; encoded[i] != '\0'; i++) {
+        char c = encoded[i];
 
         if ((c >= 'A' && c <= 'Z')) {
             encoded[i] = ((c - 'A' + 13) % 26) + 'A';
         } else if ((c >= 'a' && c <= 'z')) {
             encoded[i] = ((c - 'a' + 13) % 26) + 'a';
-        } else {
-            encoded[i] = c;
         }
     }
 
-    encoded[length] = '\0';
     return encoded;
 }
 
