bd604fb32957070f127ccbefc1dad9882f86f765
amir413 <amir.emad2007@gmail.com>
2023-06-30 22:11:08 +0300

    s

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,21 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 char* rot13(char* str) {
     int length = strlen(str);
     char* encoded = (char*)malloc((length + 1) * sizeof(char));
-    
+
     for (int i = 0; i < length; i++) {
         char c = str[i];
-        
-        if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
-            encoded[i] = c + 13;
-        else if ((c >= 'N' && c <= 'Z') || (c >= 'n' && c <= 'z'))
-            encoded[i] = c - 13;
-        else
+        char base = (c >= 'a' && c <= 'z') ? 'a' : (c >= 'A' && c <= 'Z') ? 'A' : '\0';
+
+        if (base != '\0') {
+            encoded[i] = ((c - base + 13) % 26) + base;
+        } else {
             encoded[i] = c;
+        }
     }
-    
+
     encoded[length] = '\0';
     return encoded;
+}
+
+int main() {
+    char str[] = "Hello, World!";
+    printf("Original string: %s\n", str);
+
+    char* encoded = rot13(str);
+    printf("Encoded string: %s\n", encoded);
+
+    free(encoded);
+    return 0;
 }
 
