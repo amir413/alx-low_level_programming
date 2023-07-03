96b8947d29389b8308abb53bc2e1127f69197541
amir413 <amir.emad2007@gmail.com>
2023-07-03 21:18:44 +0300

    ss

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int itr;
+	unsigned int itr;
 
-    for (itr = 0; n > 0;itr++, n--)
-    {
-        s[itr]= b;
-    }
+	for (itr = 0; n > 0;itr++, n--)
+	{
+		s[itr]= b;
+	}
 
-    return (s);
+	return (s);
 }
 
