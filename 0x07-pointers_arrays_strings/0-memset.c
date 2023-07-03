df5b9e007c8269be0b0c152ec69a506a4363a81f
amir413 <amir.emad2007@gmail.com>
2023-07-03 16:08:34 +0300

    sda

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,33 @@
-char *_memset(char *s, char b, unsigned int n) {
-    for (unsigned int i = 0; i < n; i++) {
-        s[i] = b;
-    }
-    return s;
+#include <stdio.h>
+#include <string.h>
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	i = 0;
+	while (i < size)
+	{
+		if(i %10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x" , buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
+    return (0);
 }
 
