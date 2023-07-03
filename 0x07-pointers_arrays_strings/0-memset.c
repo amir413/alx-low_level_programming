dd9d358d2c5b5333597c6e43dd0f49805e8f4c5c
amir413 <amir.emad2007@gmail.com>
2023-07-03 15:15:38 +0300

    sa

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,7 @@
-#include <stdio.h>
-
 char *_memset(char *s, char b, unsigned int n) {
     for (unsigned int i = 0; i < n; i++) {
         s[i] = b;
     }
     return s;
-}
-
-int main() {
-    char str[10] = "Hello";
-
-    printf("Before memset: %s\n", str);
-
-    _memset(str, 'A', 5);
-
-    printf("After memset: %s\n", str);
-
-    return 0;
 }
 
