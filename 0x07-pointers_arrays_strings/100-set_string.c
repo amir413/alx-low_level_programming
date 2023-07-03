dce5570b059c3f4910a809313980a3561b8a927c
amir413 <amir.emad2007@gmail.com>
2023-07-03 22:54:27 +0300

    sa

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,8 +2,8 @@
 
 /**
  * set_tring - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer char
+ * $s: pointer to pointer
+ * $to: pointer char
  */
 
 void set_string(char **s, char *to)
