cd4913a21298f3f763dcc09fe5a00cdeda664b34
amir413 <amir.emad2007@gmail.com>
2023-07-03 16:18:32 +0300

    sa

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,33 +1,41 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	i = 0;
-	while (i < size)
+	unsigned int n;
+	n = 0;
+	while (n < size)
 	{
-		if(i %10)
+		if(n %10)
 		{
 			printf(" ");
 		}
-		if (!(i % 10) && i)
+		if (!(n % 10) && n)
 		{
 			printf("\n");
 		}
-		printf("0x%02x" , buffer[i]);
-		i++;
+		printf("0x%02x" , buffer[n]);
+		n++;
 	}
 	printf("\n");
 }
 int main(void)
 {
-    char buffer[98] = {0x00};
+    char b[98] = {0x00};
 
-    simple_print_buffer(buffer, 98);
-    memset(buffer, 0x01, 95);
+    *_memset(b, 98);
+    memset(b, 0x01, 95);
     printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
-    return (0);
+    *_memset(b, 98);
+    return (s);
 }
 
