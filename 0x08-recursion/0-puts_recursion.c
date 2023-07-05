97fb24619e2a5b6dcb7ab13d11c6b2035b24ad35
amir413 <amir.emad2007@gmail.com>
2023-07-05 22:34:22 +0300

    sdsa

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _puts_recursion - Recursively prints a string followed by a new line
- * @s: pointer to the string
- * Return: void
- */
+void _puts_recursion(char *s) {
+    if (*s == '\0') {  // Check if the current character is the null terminator
+        putchar('\n');  // Print a new line
+        return;
+    }
 
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		putchar('\n');
-	}
-	else 
-	{
-		_putchar (*s);
-		_puts_recursion(s + 1);
-	}
+    putchar(*s);  // Print the current character
+    _puts_recursion(s + 1);  // Recursively call _puts_recursion with the next character
 }
 
