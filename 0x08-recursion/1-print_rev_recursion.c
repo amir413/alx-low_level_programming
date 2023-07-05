2a00da33d06eef3c29d9ac2ce153314342520d47
amir413 <amir.emad2007@gmail.com>
2023-07-06 00:51:24 +0300

    sa

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-void _print_rev_recursion(char *s)
-{
-	if (*s == '\0') {
-		return;
-	}
-	_print_rev_recursion(*s +1);
-	printf("%c",*s);
+void _print_rev_recursion(char *s) {
+    if (*s == '\0') {
+        return;
+    }
+    
+    _print_rev_recursion(s + 1);
+    printf("%c", *s);
 }
-int main()
-{
-	char str[] = "amir";
 
-	_print_rev_recursion(str);
-	printf("\n");
-
-	return (0);
+int main() {
+    char str[] = "Hello, world!";
+    
+    _print_rev_recursion(str);
+    printf("\n");
+    
+    return 0;
 }
+
