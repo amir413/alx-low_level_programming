ca34b9595ce933a71ebcdaac29f4a4cc9468ea9b
amir413 <amir.emad2007@gmail.com>
2023-07-05 22:19:31 +0300

    sas

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Recursively prints a string followed by a new line
+ * @s: Pointer to the string to be printed
+ *
+ * This function recursively prints each character of the string 's' until it
+ * reaches the null terminator character '\0', at which point it prints a new line.
+ */w
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
