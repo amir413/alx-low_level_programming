a6b7f51144cf8e576e3bcb218a5061ab5c7c418b
amir413 <amir.emad2007@gmail.com>
2023-07-12 02:06:55 +0300

    asa

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	p = malloc(size * sizeof(c));
+	if (p == o)
+	{
+		return (NULL);
+	}
+	return (0);
+}
