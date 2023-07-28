a33f89158eb08ef79fc81e2ad4a7940a065dcca2
amir413 <amir.emad2007@gmail.com>
2023-07-28 03:34:37 +0300

    sa

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -29,8 +29,8 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->next;
+		printf("[%d] %s\n" , _strlen(h -> str) , h->str ? h -> str : "(nil)");
+		h = h -> next;
 		i++;
 	}
 	return (i);
