e8a38259fe6e9b3300c4885559c9f7c722531f83
amir413 <amir.emad2007@gmail.com>
2023-07-28 03:58:06 +0300

    sa

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - determin thee length to linked lists
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	wwhile (h)
+	{
+		h = h->next;
+		i++
+	}
+	return (i);
+}
+
