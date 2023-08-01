83ad8a8f0c0ef1a060c1a8cb9d22bdb0602cb990
amir413 <amir.emad2007@gmail.com>
2023-08-01 12:20:25 +0300

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t i = 0;
 	while (h)
 	{
 		print ("%d\n", h->n);
-		num++;
 		h = h->next;
+		i++
 	}
-	return (num);
+	return (i);
 }
