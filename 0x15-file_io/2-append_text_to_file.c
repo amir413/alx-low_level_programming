9d4397152815b0fd8a241cb53eecd538d2cede60
amir413 <amir.emad2007@gmail.com>
2023-08-07 22:21:46 +0300

    s

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+90% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Try 100 GB of storage for EGP 14.99 EGP 0 for 1 month.
+2-append_text_to_file.c
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
