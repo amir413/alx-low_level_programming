c6b0735467a5672fdd02dae52914edd0258c796f
amir413 <amir.emad2007@gmail.com>
2023-10-05 09:14:14 +0300

    s

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-gfgdcfbdgsdfafadffafa
-afasdfadasfsdgfsg
+readme
---
+++ amir.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlin.h>
+
+void hello(void){
+	printf("hello world\n");
+}
+int main(void){
+	hello();
+}
